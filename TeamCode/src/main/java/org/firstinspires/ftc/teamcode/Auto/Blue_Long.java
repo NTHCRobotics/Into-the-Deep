def58e09584d32f69df59b28bfb4abc4c563de4f
FireGalaxy144 <89485180+FireGalaxy144@users.noreply.github.com>
2024-10-14 22:00:53 -0500

    Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Blue_Long.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+
+
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+
+@Autonomous(name="BlueLong", group="Robot")
+
+
+public class    Blue_Long extends LinearOpMode {
+
+    private DcMotorEx wheelFL;
+    private DcMotorEx wheelFR;
+    private DcMotorEx wheelBL;
+    private DcMotorEx wheelBR;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Motors
+        wheelFL = hardwareMap.get(DcMotorEx.class, "wheelFL");
+        wheelFR = hardwareMap.get(DcMotorEx.class, "wheelFR");
+        wheelBL = hardwareMap.get(DcMotorEx.class, "wheelBL");
+        wheelBR = hardwareMap.get(DcMotorEx.class, "wheelBR");
+
+
+        wheelFL.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+        wheelFR.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBL.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBR.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+
+
+        wheelFL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelFR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+// Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
+        Action trajectoryAction1;
+        trajectoryAction1 = drive.actionBuilder(drive.pose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3)
+                .build();
+
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red_Short.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+
+@Autonomous(name="RedShort", group="Robot")
+
+
+public class    Red_Short extends LinearOpMode {
+
+    private DcMotorEx wheelFL;
+    private DcMotorEx wheelFR;
+    private DcMotorEx wheelBL;
+    private DcMotorEx wheelBR;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Motors
+        wheelFL = hardwareMap.get(DcMotorEx.class, "wheelFL");
+        wheelFR = hardwareMap.get(DcMotorEx.class, "wheelFR");
+        wheelBL = hardwareMap.get(DcMotorEx.class, "wheelBL");
+        wheelBR = hardwareMap.get(DcMotorEx.class, "wheelBR");
+
+
+        wheelFL.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+        wheelFR.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBL.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBR.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+
+
+        wheelFL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelFR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+// Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
+        Action trajectoryAction1;
+        trajectoryAction1 = drive.actionBuilder(drive.pose)
+                .lineToX(45)
+                .build();
+
+
+
+
+
+
+
+
+
+
+    }
+}
